53a08d4db17576be64fe814e140fc93d2acf832a
cen24 <cen24@njit.edu>
2022-03-07 23:27:13 -0500

    tf iaac file

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+#below creates a simple ec2 instance called "coach32server". Make sure aws region matches region for ami id in aws console
+resource "aws_instance" "webserver" {
+  ami           = "ami-0c293f3f676ec4f90"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "coach32server"
+  }
+}
+
+#below creates a simple public s3 bucket called "coach32serverbucket".
+resource "aws_s3_bucket" "webserverbucket" {
+   bucket = "coach32serverbucket" 
+}
